3bbd928a09d344b49c937ad044bfa3efcab4c150
Eric Jackson <ejackson@ashevillenc.gov>
2018-10-23 10:33:41 -0400

    Changed reviewable to depend only on whether active, has email and is in a list of employment status types

--- resolve/getEmployee.js
+++ resolve/getEmployee.js
@@ -1,5 +1,29 @@
 const sql = require('mssql');
 
+const reviewableTypes = ['CA', 'FT', 'IN', 'PB', 'PN'];
+const isReviewable = (e) => {
+  // return (
+  //   e.Active === 'A' && e.Position !== null && e.Position !== '' &&
+  //   e.Emp_Email !== null && e.Emp_Email !== ''
+  // );
+  return (
+    e.Active === 'A' &&
+    e.Emp_Email !== null && e.Emp_Email !== '' &&
+    reviewableTypes.includes(e.FT_Status)
+  );
+};
+
+const notReviewableReason = (e) => {
+  let reason = null;
+  if (!isReviewable(e)) {
+    if (e.Active !== 'A') reason = 'Inactive';
+    else if (!reviewableTypes.includes(e.FT_Status)) reason = 'Non-included employee type';
+    else if (e.Position === null || e.Position === '') reason = 'No position';
+    else reason = 'Employee not registered for Employee Check-in';
+  }
+  return reason;
+};
+
 const getEmployee = (id, pool, logger) => {
   return pool.request()
     .input('UserEmpID', sql.Int, id)
@@ -10,6 +34,7 @@ const getEmployee = (id, pool, logger) => {
       return Promise.resolve({
         id,
         active: e.Active,
+        ft_status: e.FT_Status,
         name: e.Employee,
         email: e.Emp_Email,
         position: e.Position,
@@ -17,8 +42,9 @@ const getEmployee = (id, pool, logger) => {
         division: e.Division,
         current_review: e.CurrentReview,
         last_reviewed: lastRev,
-        reviewable: e.Active && e.Position !== null && e.Position !== '',
+        reviewable: isReviewable(e),
         review_by: new Date(e.ReviewBy).toISOString(),
+        not_reviewable_reason: notReviewableReason(e),
         supervisor_id: e.SupID,
         supervisor_name: e.Supervisor,
         supervisor_email: e.Sup_Email,
