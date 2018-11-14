831ecd435f0b79a0c3a71e9aeee733d6b61c9b2c
Eric Jackson <ejackson@ashevillenc.gov>
2018-11-14 15:33:55 -0500

    more cleanup

--- resolve/employee_queries.js
+++ resolve/employee_queries.js
@@ -1,10 +1,6 @@
-const getEmployee = require('./utilities/get_employee_info.js');
+const { getEmployee, getSubordinates } = require('./utilities/get_employee_info.js');
 const operationIsAllowed = require('./utilities/operation_is_allowed');
-const { isReviewable, notReviewableReason } = require('./utilities/reviewable');
 
-/* *******************
- * 'employee resolver
- * *******************/
 const employee = (obj, args, context) => {
   const pool = context.pool;
   if (Object.prototype.hasOwnProperty.call(args, 'id')) {
@@ -22,61 +18,13 @@ const employee = (obj, args, context) => {
 };
 
 const employees = (obj, args, context) => {
-  console.log('In employees');
   const pool = context.pool;
   const whPool = context.whPool;
-  const id = obj.id;// AUTH HERE before doing the rest of the query.
-  const employeesById = {};
+  const id = obj.id;
   return operationIsAllowed(id, context)
   .then(isAllowed => {
     if (isAllowed) {
-      const eIds = [];
-      return whPool.query('select * from internal.employees_main_view where sup_id = $1', [id])
-      .then(result => {
-        result.rows.filter(e => {
-          return e.active === 'A';
-        }).forEach(e => {
-          eIds.push(e.emp_id);
-          const emp = {
-            id: e.emp_id,
-            active: e.active,
-            ft_status: e.ft_status,
-            name: e.employee,
-            email: e.emp_email,
-            position: e.position,
-            department: e.department,
-            division: e.division,
-            current_review: null,
-            last_reviewed: null,
-            reviewable: isReviewable(e),
-            review_by: null,
-            not_reviewable_reason: notReviewableReason(e),
-            supervisor_id: e.sup_id,
-            supervisor_name: e.supervisor,
-            supervisor_email: e.sup_email,
-            employees: null,
-            reviews: null,
-          };
-          employeesById[e.emp_id] = emp;
-        });
-        const employeesReviewStatusQuery = `
-        SELECT employee_id,
-        MAX( CASE WHEN status = 'Closed' THEN status_date ELSE null END) as last_reviewed,
-        MAX( CASE WHEN status <> 'Closed' THEN review_id ELSE null END) as current_review
-        FROM reviews.reviews 
-        WHERE employee_id = ANY($1) GROUP BY employee_id
-        `;
-        return pool.query(employeesReviewStatusQuery, [eIds])
-        .then(res => {
-          console.log(JSON.stringify(res.rows));
-          return res.rows.map(r => {
-            const e = employeesById[r.employee_id];
-            e.current_review = r.current_review;
-            e.last_reviewed = (r.last_reviewed === null) ? null : new Date(r.last_reviewed).toISOString();
-            return e;
-          });
-        });
-      });
+      return getSubordinates(obj.id, pool, whPool, context.logger);
     }
     throw new Error('Employees query not allowed');
   });
--- resolve/utilities/get_employee_info.js
+++ resolve/utilities/get_employee_info.js
@@ -1,50 +1,47 @@
 const { isReviewable, notReviewableReason } = require('./reviewable');
 
+const employeesReviewStatusQuery = `
+  SELECT employee_id,
+    MAX( CASE WHEN status = 'Closed' THEN status_date ELSE null END) as last_reviewed,
+    MAX( CASE WHEN status <> 'Closed' THEN review_id ELSE null END) as current_review
+  FROM reviews.reviews 
+  WHERE employee_id = ANY($1) GROUP BY employee_id
+`;
+
+const loadEmployee = (e) => {
+  return {
+    id: e.emp_id,
+    active: e.active,
+    ft_status: e.ft_status,
+    name: e.employee,
+    email: e.emp_email,
+    position: e.position,
+    department: e.department,
+    division: e.division,
+    current_review: null,
+    last_reviewed: null,
+    reviewable: isReviewable(e),
+    review_by: null,
+    not_reviewable_reason: notReviewableReason(e),
+    supervisor_id: e.sup_id,
+    supervisor_name: e.supervisor,
+    supervisor_email: e.sup_email,
+    employees: null,
+    reviews: null,
+  };
+};
+
 const getEmployee = (id, pool, whPool, logger) => {
-  console.log('I am in getEmployee');
   return whPool.query('select * from internal.employees_main_view where emp_id = $1', [id])
     .then(result => {
-      const e = result.rows[0];
-      // const lastRev = (e.LastReviewed === null) ? null : new Date(e.LastReviewed).toISOString();
-      return Promise.resolve({
-        id,
-        active: e.active,
-        ft_status: e.ft_status,
-        name: e.employee,
-        email: e.emp_email,
-        position: e.position,
-        department: e.department,
-        department_id: e.dept_id,
-        division: e.division,
-        division_id: e.div_id,
-        current_review: null, // CurrentReview
-        last_reviewed: null, // Last reviewed
-        reviewable: isReviewable(e),
-        review_by: null, // new Date(e.ReviewBy).toISOString(),
-        not_reviewable_reason: notReviewableReason(e),
-        supervisor_id: e.sup_id,
-        supervisor_name: e.supervisor,
-        supervisor_email: e.sup_email,
-        employees: [],
-        reviews: null,
-      });
-    })
-    .then(employee => {
-      const employeesReviewStatusQuery = `
-        SELECT employee_id,
-        MAX( CASE WHEN status = 'Closed' THEN status_date ELSE null END) as last_reviewed,
-        MAX( CASE WHEN status <> 'Closed' THEN review_id ELSE null END) as current_review
-        FROM reviews.reviews 
-        WHERE employee_id = $1 GROUP BY employee_id
-        `;
-      return pool.query(employeesReviewStatusQuery, [id])
+      const employee = loadEmployee(result.rows[0]);
+      return pool.query(employeesReviewStatusQuery, [[id]])
       .then(res => {
         const r = res.rows[0];
-        const augEmployee = Object.assign({}, employee, {
+        return Object.assign({}, employee, {
           current_review: r.current_review,
           last_reviewed: (r.last_reviewed === null) ? null : new Date(r.last_reviewed).toISOString(),
         });
-        return augEmployee;
       });
     })
     .catch(err => {
@@ -53,4 +50,31 @@ const getEmployee = (id, pool, whPool, logger) => {
     });
 };
 
-module.exports = getEmployee;
+const getSubordinates = (id, pool, whPool, logger) => {
+  const employeesById = {};
+  const eIds = [];
+  return whPool.query('select * from internal.employees_main_view where sup_id = $1', [id])
+  .then(result => {
+    result.rows.filter(e => {
+      return e.active === 'A';
+    }).forEach(e => {
+      eIds.push(e.emp_id);
+      employeesById[e.emp_id] = loadEmployee(e);
+    });
+    return pool.query(employeesReviewStatusQuery, [eIds])
+    .then(res => {
+      return res.rows.map(r => {
+        const e = employeesById[r.employee_id];
+        e.current_review = r.current_review;
+        e.last_reviewed = (r.last_reviewed === null) ? null : new Date(r.last_reviewed).toISOString();
+        return e;
+      });
+    });
+  });
+};
+
+module.exports = {
+  getEmployee,
+  getSubordinates,
+};
+
