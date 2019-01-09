ca8b6b751941168f86619b727bce994cf5be2f0d
Eric Jackson <ejackson@ashevillenc.gov>
2019-01-09 13:41:58 -0500

    Fixed bug that bombs when employee email is null

--- resolve/utilities/reviewable.js
+++ resolve/utilities/reviewable.js
@@ -15,7 +15,7 @@ const notReviewableReason = (e) => {
     if (e.active !== 'A') reason = 'Inactive';
     else if (!reviewableTypes.includes(e.ft_status)) reason = 'Non-included employee type';
     else if (e.position === null || e.position === '') reason = 'No position';
-    else if (!e.emp_email.trim().toLowerCase().endsWith('ashevillenc.gov')) reason = 'City of Asheville email required';
+    else if (e.emp_email === null || !e.emp_email.trim().toLowerCase().endsWith('ashevillenc.gov')) reason = 'City of Asheville email required';
     else reason = 'Employee not registered for Employee Check-in';
   }
   return reason;
